b095001d1ee4b692014a877406bfb77b6fcd5c08
LY（退网/offline） <51789698+Young-Lord@users.noreply.github.com>
2024-06-04 19:15:24 +0800

    bump

--- query_rdp_ip.ps1
+++ query_rdp_ip.ps1
@@ -1,15 +1,22 @@
-﻿$xml='<QueryList>
-  <Query Id="0" Path="Security">
-    <Select Path="Security">*[System[(EventID=4624)]]</Select>
-  </Query>
+# https://github.com/Young-Lord/rdp-dump-ip-from-event-log/blob/master/query_rdp_ip.ps1
+# ref: https://powershell.org/2019/08/a-better-way-to-search-events/
+# ref: https://stackoverflow.com/questions/68441505/powershell-how-to-read-eventdata-part-of-the-xml
+
+$xml='<QueryList>
+<Query Id="0" Path="Security">
+  <Select Path="Security">*[System[(EventID=4624)]] and *[EventData[Data[@Name="IpAddress"]!="-"]] and *[EventData[Data[@Name="TargetUserName"]!="ANONYMOUS LOGON"]]</Select>
+</Query>
 </QueryList>'
-#Write-Host '登录时间','登录类型','登录账号','登录IP地址'
-foreach ($event in Get-WinEvent -FilterXml $xml) {
-    try { $time=$event.TimeCreated
-    $type= [regex]::matches($event.Message, '登录类型:(.+)')   | %{$_.Groups[1].Value.Trim()}
-    $user=([regex]::matches($event.Message, '帐户名称?:(.+)')  | %{$_.Groups[1].Value.Trim()})[1]
-    $IP=  [regex]::matches($event.Message, '源网络地址:(.+)') | %{$_.Groups[1].Value.Trim()}
-    if ($IP -ne "-"){ Write-Host $time,$user,$type,$IP }
+
+$result=Get-WinEvent -FilterXml $xml -MaxEvents 100 | ForEach-Object {
+    $eventXml = ([xml]$_.ToXml()).Event
+    [PsCustomObject]@{
+        time=$_.TimeCreated
+        TargetUserName = ($eventXml.EventData.Data | Where-Object { $_.Name -eq 'TargetUserName' }).'#text'
+        IpAddress = ($eventXml.EventData.Data | Where-Object { $_.Name -eq 'IpAddress' }).'#text'
+        WorkstationName = ($eventXml.EventData.Data | Where-Object { $_.Name -eq 'WorkstationName' }).'#text'
+        LogonType = ($eventXml.EventData.Data | Where-Object { $_.Name -eq 'LogonType' }).'#text'
     }
-    catch { }
 }
+
+$result | Format-Table
